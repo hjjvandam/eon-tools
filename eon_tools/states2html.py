2e707f2ac307d92256311f31779a64704e0cdbeb
hjjvandam <hvandam@bnl.gov>
2023-03-13 15:25:36 -0400

    Added the option write movies of the reaction processes instead of snapshots of reactants, transition states, and products.

--- eon_tools/states2html.py
+++ eon_tools/states2html.py
@@ -3,6 +3,7 @@ Convert a "states" directory to an HTML webpage
 '''
 from ase.io import read
 from ase.io import write
+from ase.neb import NEB
 
 def find_states(statespath):
     '''
@@ -38,7 +39,7 @@ def find_processes(statespath,statename):
             table.append(process[0])
     return table
 
-def convert_con2png(statespath,statename,processname):
+def convert_con2img(statespath,statename,processname,movies):
     '''
     Convert the configuration files to image files
 
@@ -51,14 +52,24 @@ def convert_con2png(statespath,statename,processname):
     fn_reactant = statespath+"/"+statename+"/procdata/reactant_"+processname
     fn_saddle   = statespath+"/"+statename+"/procdata/saddle_"+processname
     fn_product  = statespath+"/"+statename+"/procdata/product_"+processname
-    structure   = read(fn_reactant+".con")
-    write(fn_reactant+".png",structure)
-    structure   = read(fn_saddle+".con")
-    write(fn_saddle+".png",structure)
-    structure   = read(fn_product+".con")
-    write(fn_product+".png",structure)
+    fn_reaction = statespath+"/"+statename+"/procdata/reaction_"+processname
+    structure_r = read(fn_reactant+".con")
+    structure_s = read(fn_saddle+".con")
+    structure_p = read(fn_product+".con")
+    if movies:
+        images = [structure_r]
+        images += [structure_r.copy() for i in range(8)]
+        images += [structure_p]
+        neb = NEB(images)
+        neb.interpolate()
+        write(fn_reaction+".gif",images)
+    else:
+        write(fn_reactant+".png",structure_r)
+        write(fn_saddle+".png",structure_s)
+        write(fn_product+".png",structure_p)
+    
 
-def html_process(fp_html,statespath,statename,processname):
+def html_process(fp_html,statespath,statename,processname,movies):
     '''
     Add the process images to the HTML file
 
@@ -67,19 +78,23 @@ def html_process(fp_html,statespath,statename,processname):
     fn_reactant = statespath+"/"+statename+"/procdata/reactant_"+processname+".png"
     fn_saddle   = statespath+"/"+statename+"/procdata/saddle_"+processname+".png"
     fn_product  = statespath+"/"+statename+"/procdata/product_"+processname+".png"
+    fn_reaction = statespath+"/"+statename+"/procdata/reaction_"+processname+".gif"
     fp_html.write("<TR><TD>")
     fp_html.write(statename)
     fp_html.write("</TD><TD>")
     fp_html.write(processname)
     fp_html.write("</TD><TD>")
-    fp_html.write("<IMG SRC=\""+fn_reactant+"\">")
-    fp_html.write("</TD><TD>")
-    fp_html.write("<IMG SRC=\""+fn_saddle+"\">")
-    fp_html.write("</TD><TD>")
-    fp_html.write("<IMG SRC=\""+fn_product+"\">")
+    if movies:
+        fp_html.write("<IMG SRC=\""+fn_reaction+"\">")
+    else:
+        fp_html.write("<IMG SRC=\""+fn_reactant+"\">")
+        fp_html.write("</TD><TD>")
+        fp_html.write("<IMG SRC=\""+fn_saddle+"\">")
+        fp_html.write("</TD><TD>")
+        fp_html.write("<IMG SRC=\""+fn_product+"\">")
     fp_html.write("</TD></TR>\n")
 
-def doit(statespath):
+def doit(statespath,movies):
     '''
     Go through the whole thing
     '''
@@ -91,8 +106,8 @@ def doit(statespath):
     for state in stateslist:
         processlist = find_processes(statespath,state)
         for process in processlist:
-            #convert_con2png(statespath,state,process)
-            html_process(fp_html,statespath,state,process)
+            convert_con2img(statespath,state,process,movies)
+            html_process(fp_html,statespath,state,process,movies)
     fp_html.write("</TABLE>\n")
     fp_html.write("</BODY>\n")
     fp_html.write("</HTML>\n")
@@ -106,9 +121,10 @@ def arguments():
     '''
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("pathname", type=str, help="the pathname for the states directory")
+    parser.add_argument("pathname", type=str, help="the pathname for the states directory, i.e. where the file \"state_table\" lives")
+    parser.add_argument("--movies", dest="movies", default=False, action='store_const', const=True, help="if provided generate movies of the reaction processes")
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = arguments()
-    doit(args.pathname)
+    doit(args.pathname,args.movies)
